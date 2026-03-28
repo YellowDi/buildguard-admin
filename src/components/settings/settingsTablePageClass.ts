4b5062885190b1cd1f7d94ee3cb2e4e917896e60
Rolly <yellowdi@me.com>
2026-03-29 00:35:52 +0800

    Increase settings table body row height

--- src/components/settings/settingsTablePageClass.ts
+++ src/components/settings/settingsTablePageClass.ts
@@ -1,3 +1,3 @@
-/** 设置浮窗内表格：表头 38px，tbody 行 40px */
+/** 设置浮窗内表格：表头 38px，tbody 行 42px */
 export const SETTINGS_TABLE_PAGE_CLASS =
-  "text-[13px] [&_thead_th]:h-[38px] [&_thead_th]:px-2.5 [&_thead_th]:py-0 [&_thead_th]:align-middle [&_tbody_td]:h-10 [&_tbody_td]:px-2.5 [&_tbody_td]:py-0 [&_tbody_td]:align-middle [&_tbody_td]:!border-l-0"
+  "text-[13px] [&_thead_th]:h-[38px] [&_thead_th]:px-2.5 [&_thead_th]:py-0 [&_thead_th]:align-middle [&_tbody_td]:h-[42px] [&_tbody_td]:px-2.5 [&_tbody_td]:py-0 [&_tbody_td]:align-middle [&_tbody_td]:!border-l-0"
