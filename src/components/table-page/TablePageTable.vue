f32f3cc126d3050a2d267da126be6026929e3e49
Rolly <yellowdi@me.com>
2026-04-11 20:56:05 +0800

    Tighten table horizontal scrollbar spacing

--- src/components/table-page/TablePageTable.vue
+++ src/components/table-page/TablePageTable.vue
@@ -1819,28 +1819,32 @@ onBeforeUnmount(() => {
         {{ summary }}
       </div>
 
-      <div
-        v-if="hasVisibleBodyRows && horizontalOverflow"
-        :class="horizontalScrollbarSectionClassName"
-        :style="horizontalScrollbarSectionStyle"
-      >
         <div
-          ref="horizontalScrollbarTrackRef"
-          class="group relative h-4 w-full touch-none select-none"
-          @pointerdown="handleHorizontalScrollbarTrackPointerDown"
+          v-if="hasVisibleBodyRows && horizontalOverflow"
+          :class="horizontalScrollbarSectionClassName"
+          :style="horizontalScrollbarSectionStyle"
         >
-          <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-border" />
           <div
-            data-table-hscroll-thumb="true"
-            :class="[
-              'absolute top-1/2 -translate-y-1/2 rounded-full bg-border-hover transition-[height,background-color] duration-150 ease-out',
-              horizontalScrollbarDrag
-                ? 'h-1.5 cursor-grabbing bg-border-hover'
-                : 'h-px cursor-grab group-hover:h-1.5 group-hover:bg-border-hover active:cursor-grabbing active:h-1.5',
-            ]"
-            :style="horizontalScrollbarThumbStyle"
-            @pointerdown.stop="handleHorizontalScrollbarThumbPointerDown"
-          />
+            class="relative h-px w-full overflow-visible"
+          >
+            <div
+              ref="horizontalScrollbarTrackRef"
+              class="group absolute inset-x-0 top-1/2 h-4 -translate-y-1/2 touch-none select-none"
+            @pointerdown="handleHorizontalScrollbarTrackPointerDown"
+          >
+            <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-border" />
+            <div
+              data-table-hscroll-thumb="true"
+              :class="[
+                'absolute top-1/2 -translate-y-1/2 rounded-full bg-border-hover transition-[height,background-color] duration-150 ease-out',
+                horizontalScrollbarDrag
+                  ? 'h-1.5 cursor-grabbing bg-border-hover'
+                  : 'h-px cursor-grab group-hover:h-1.5 group-hover:bg-border-hover active:cursor-grabbing active:h-1.5',
+              ]"
+              :style="horizontalScrollbarThumbStyle"
+              @pointerdown.stop="handleHorizontalScrollbarThumbPointerDown"
+            />
+          </div>
         </div>
       </div>
     </div>
