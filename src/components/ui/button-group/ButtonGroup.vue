3e3b0b5aa91f205181a1324e4c68a634211e751d
Rolly <yellowdi@me.com>
2026-05-01 00:49:37 +0800

    Add button group variants and separator components

--- src/components/ui/button-group/ButtonGroup.vue
+++ src/components/ui/button-group/ButtonGroup.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from "vue"
+import type { ButtonGroupVariants } from "."
 import { cn } from "@/lib/utils"
+import { buttonGroupVariants } from "."
 
 const props = withDefaults(defineProps<{
   class?: HTMLAttributes["class"]
-  orientation?: "horizontal" | "vertical"
+  orientation?: ButtonGroupVariants["orientation"]
 }>(), {
   orientation: "horizontal",
 })
@@ -15,13 +17,7 @@ const props = withDefaults(defineProps<{
     data-slot="button-group"
     role="group"
     :data-orientation="props.orientation"
-    :class="cn(
-      'inline-flex isolate w-fit rounded-md shadow-xs',
-      'data-[orientation=horizontal]:flex-row data-[orientation=horizontal]:[&>*:not(:first-child)]:-ml-px data-[orientation=horizontal]:[&>*:not(:first-child)]:rounded-l-none data-[orientation=horizontal]:[&>*:not(:last-child)]:rounded-r-none',
-      'data-[orientation=vertical]:flex-col data-[orientation=vertical]:[&>*:not(:first-child)]:-mt-px data-[orientation=vertical]:[&>*:not(:first-child)]:rounded-t-none data-[orientation=vertical]:[&>*:not(:last-child)]:rounded-b-none',
-      '[&>*]:relative [&>*]:shadow-none [&>*]:focus-visible:z-10',
-      props.class,
-    )"
+    :class="cn(buttonGroupVariants({ orientation: props.orientation }), props.class)"
   >
     <slot />
   </div>
--- src/components/ui/button-group/index.ts
+++ src/components/ui/button-group/index.ts
@@ -1,1 +1,28 @@
+import type { VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
+
 export { default as ButtonGroup } from "./ButtonGroup.vue"
+export { default as ButtonGroupSeparator } from "./ButtonGroupSeparator.vue"
+export { default as ButtonGroupText } from "./ButtonGroupText.vue"
+
+export const buttonGroupVariants = cva(
+  [
+    "inline-flex isolate w-fit rounded-md shadow-xs",
+    "[&>*]:relative [&>*]:shadow-none [&>*]:focus-visible:z-10",
+  ],
+  {
+    variants: {
+      orientation: {
+        horizontal:
+          "flex-row [&>*:not(:first-child)]:-ml-px [&>*:not(:first-child)]:rounded-l-none [&>*:not(:last-child)]:rounded-r-none",
+        vertical:
+          "flex-col [&>*:not(:first-child)]:-mt-px [&>*:not(:first-child)]:rounded-t-none [&>*:not(:last-child)]:rounded-b-none",
+      },
+    },
+    defaultVariants: {
+      orientation: "horizontal",
+    },
+  },
+)
+
+export type ButtonGroupVariants = VariantProps<typeof buttonGroupVariants>
