957afe8d1360ade4e04791d6a5e0e403d0928335
Rolly <yellowdi@me.com>
2026-06-02 15:05:15 +0800

    Lower detail layout breakpoint

--- src/components/loading/DetailPageLoading.vue
+++ src/components/loading/DetailPageLoading.vue
@@ -16,7 +16,7 @@ import { Skeleton } from "@/components/ui/skeleton"
       </div>
     </div>
 
-    <div class="grid min-h-0 flex-1 grid-cols-1 gap-4 px-0 xl:grid-cols-[minmax(0,1.2fr)_minmax(20rem,0.8fr)] xl:gap-8">
+    <div class="grid min-h-0 flex-1 grid-cols-1 gap-4 px-0 min-[960px]:grid-cols-[minmax(0,1.2fr)_minmax(20rem,0.8fr)] min-[960px]:gap-8">
       <div class="min-w-0">
         <div
           v-for="section in 2"
